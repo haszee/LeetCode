cb0ddda931b2da2af88917044b9db0f1f75903e5
haszee <146680970+haszee@users.noreply.github.com>
2024-05-31 17:11:15 -0600

    Time: 57 ms (26.05%) | Memory: 48.6 MB (65.85%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let object = {
+        toBe(otherVal) {
+            if (val === otherVal) {
+                return true;
+            } else {
+                throw new Error('Not Equal');
+            };
+        },
+
+        notToBe(otherVal) {
+            if (val !== otherVal) return true;
+            else throw new Error('Equal');
+        }
+    }
+    return object;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
