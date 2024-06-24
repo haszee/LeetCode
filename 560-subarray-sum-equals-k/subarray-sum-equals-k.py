ad5fa35a4b1d720caae0b1b3d9b4ef6bca103918
haszee <146680970+haszee@users.noreply.github.com>
2024-06-23 23:49:59 -0600

    Time: 209 ms (98.90%) | Memory: 19.5 MB (59.91%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,13 +5,17 @@ class Solution:
         count = 0
 
         if len(nums) == 0:
-            return 0
+            return 0 # count is 0 if list is empty
 
         for num in nums:
             currSum += num
 
-            if (currSum - k) in prefHash:
-                count += prefHash[currSum-k]
+            # x = currSum - k
+            # determine whether the value x is present in Hashtable
+            # if present then that means there is a subarray of value x which can be subtracted from current array to create another subarray with a summed value of k
+            # since you can create a value of k then that means 
+            if (currSum - k) in prefHash: 
+                count += prefHash[currSum-k] 
             
             if currSum in prefHash:
                 prefHash[currSum] += 1
