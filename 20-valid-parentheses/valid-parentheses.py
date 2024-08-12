f2b6b18db3368f67f890f7b5641fc26bc34f1ed1
haszee <146680970+haszee@users.noreply.github.com>
2024-08-11 19:20:44 -0600

    Time: 37 ms (46.85%) | Memory: 16.3 MB (99.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,30 +2,32 @@ class Solution:
     def isValid(self, s: str) -> bool:
         brackets = []
 
-        if s[0] == ']' or s[0] == ')' or s[0] == '}':
-            return False
+        # if s[0] == ']' or s[0] == ')' or s[0] == '}':
+        #     return False
 
         for bracket in s:
             if bracket == '[' or bracket == '(' or bracket == '{':
                 brackets.append(bracket)
+            elif len(brackets) == 0 and (bracket == ']' or bracket == ')' or bracket == '}'):
+                return False
             elif bracket == ']':
-                if len(brackets) == 0:
-                    return False
-                elif brackets[-1] != '[':
+                # if len(brackets) == 0:
+                #     return False
+                if brackets[-1] != '[':
                     return False
                 else:
                     brackets.pop()
             elif bracket == ')':
-                if len(brackets) == 0:
-                    return False
-                elif brackets[-1] != '(' or len(brackets) == 0:
+                # if len(brackets) == 0:
+                #     return False
+                if brackets[-1] != '(' or len(brackets) == 0:
                     return False
                 else:
                     brackets.pop()
             elif bracket == '}':
-                if len(brackets) == 0:
-                    return False
-                elif brackets[-1] != '{' or len(brackets) == 0:
+                # if len(brackets) == 0:
+                #     return False
+                if brackets[-1] != '{' or len(brackets) == 0:
                     return False
                 else:
                     brackets.pop()
