a20488ebde7a8d552709c72eb448a24edcace065
haszee <146680970+haszee@users.noreply.github.com>
2024-08-15 17:31:33 -0600

    Time: 746 ms (5.01%) | Memory: 20.4 MB (78.76%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,27 +1,27 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         cashier = []
-
+        
         for bill in bills:
             if bill == 5:
                 cashier.append(bill)
             elif bill == 10:
-                try:
+                if 5 in cashier:
                     cashier.remove(5)
-                    cashier.append(bill)
-                except:
+                    cashier.append(10)
+                else:
                     return False
             else:
-                try:
-                    cashier.remove(10)
+                if 5 in cashier and 10 in cashier:
                     cashier.remove(5)
-                except:
-                    try:
-                        cashier.remove(5)
-                        cashier.remove(5)
-                        cashier.remove(5)
-                    except:
-                        return False
+                    cashier.remove(10)
+                elif 10 not in cashier and cashier.count(5)>=3:
+                    cashier.remove(5)
+                    cashier.remove(5)
+                    cashier.remove(5)
+                else:
+                    return False
+                
         return True
-        
+
 
